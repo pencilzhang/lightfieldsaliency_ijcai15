b2ab509f5612d874ee631b227601acfb37cc70c9
Jun Zhang <zhangjun1126@gmail.com>
2015-12-28 22:45:52 +0800

    update the comments

--- code/util/CalWeightedContrast.m
+++ code/util/CalWeightedContrast.m
@@ -1,10 +1,10 @@
 function wCtr = CalWeightedContrast(feaDistM, posDistM, bgProb)
 %% Calculate background probability weighted contrast
 
-spaSigma = 0.67;     %sigma for spatial weight
-posWeight = Dist2WeightMatrix(posDistM, spaSigma);  %posWeight = Wspa(p,pi)% exp(-d^2/(2*sigma^2));posDistM³¬ÏñËØ¼ä¿Õ¼ä¾àÀë
+spaSigma = 0.67;     
+posWeight = Dist2WeightMatrix(posDistM, spaSigma); 
 
-wCtr = (feaDistM .* posWeight * bgProb);   %(8) dapp(p,pi)*Wspa(p,pi)*Wibg£» dapp(p,pi)=colDistM ³¬ÏñËØ¼äÑÕÉ«²î
+wCtr = (feaDistM .* posWeight * bgProb);  
 wCtr = (wCtr - min(wCtr)) / (max(wCtr) - min(wCtr) + eps);
 
 %post-processing for cleaner fg cue
