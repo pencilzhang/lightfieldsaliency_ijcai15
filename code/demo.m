185844b1c5263f65cf4526db4b930bdbf7b2f481
Jun Zhang <zhangjun1126@gmail.com>
2018-06-04 09:41:01 +0800

    release the source demo

--- code/demo.m
+++ code/demo.m
@@ -11,15 +11,15 @@ saveBLS = false;        % save background slices
 
 
 %% all-focus image;depth map;focal stack; focusness map
-SRC_afi = '../data/SRCalfcim';          %Path of all-focus image
+SRC_afi = '../Data/SRCalfcim';          %Path of all-focus image
 srcSuffix = '.jpg';                  %suffix for your input image
-SRC_depth = '../data/SRCdep';           %Path of depth map
+SRC_depth = '../Data/SRCdep';           %Path of depth map
 srcSuffix_depth = '.bmp';
-SRC_fcness = '../data/SRCfcness';       %Path of focness stacks
+SRC_fcness = '../Data/SRCfcness';       %Path of focness stacks
 SRC_bg = 'Result/BG';                    %Path for saving background image
 
-RES_Sali = '../results/Salient_map';     %Path for saving final saliency maps
-if ~exist(RES_Sali,'file');
+RES_Sali = 'salmap';     %Path for saving final saliency maps
+if ~exist(RES_Sali,'file')
     mkdir(RES_Sali);
 end
 
@@ -34,7 +34,8 @@ for k=1:length(files)
     disp(k);
     srcName = files(k).name;
     noSuffixName = srcName(1:end-length(srcSuffix));       
-    
+    smapName = fullfile(RES_Sali, strcat(noSuffixName, '.png'));
+
     srcImg = imread(fullfile(SRC_afi, srcName));        % all focs image
     
     if doFrameRemoving
@@ -51,15 +52,55 @@ for k=1:length(files)
     srcImg_depth = imresize(srcImg_depth,[360,360]);
     
     spnumber = round( h * w / pixNumInSP );     %super-pixel number for current image
-    [optsalMap,pixelList] = generate_prior_map(SRC_fcness,SRC_bg,noFrameImg,srcImg_depth,noSuffixName,alpha,spnumber,useSP,saveBLS);
+    [idxImg, adjcMatrix, pixelList] = SLIC_Split(noFrameImg, spnumber);
     
+    %% Get super-pixel properties
+    meanRgbCol = GetMeanSuperPixel(noFrameImg, pixelList);%all-focus images
+    meanLabCol = colorspace('Lab<-', double(meanRgbCol)/255);
+    meanPos = GetNormedMeanPos(pixelList, h, w);%centers of superpixel
+    bdIds = GetBndPatchIds(idxImg);
+    posDistM = GetDistanceMatrix(meanPos);
+    
+    %% select Background slice from focal stack
+    fcsName = strcat(noSuffixName, '__refocus_');
+    fcness_src = fullfile(SRC_fcness,fcsName);
+    BGim = find_bgslice(fcness_src);
+    if saveBLS
+        bgName = fullfile(BG, strcat(noSuffixName,'.png'));
+        imwrite(BGim, bgName);  %save BG image
+    end
+    
+    [hb, wb, chnb] = size(BGim);
+    spNumb = length(pixelList);
+    location_prior = zeros(spNumb, chnb);
+    meanBgIm = GetMeanSuperPixel(BGim, pixelList);
+    BgDistM = GetDistanceMatrix(meanBgIm);
+    imcentropos = measucentroid(noFrameImg,  h, w);
+    for i=1:spNumb
+        location_prior(i) = norm(imcentropos-meanPos(i,:));
+    end
+    bdConSigma = 1;
+    bgfocus = 1-(exp((-meanBgIm.^2/(2 * bdConSigma * bdConSigma)) .* location_prior));
+    
+    %% depth and color contrast maps
+    srcName_depth = files_depth(k).name;
+    srcImg_depth = imread(fullfile(SRC_depth, srcName_depth));
+    srcImg_depth = imresize(srcImg_depth,[360,360]);
+    meanDepth = GetMeanSuperPixel(srcImg_depth, pixelList);
+    
+    colDistM = GetDistanceMatrix(meanLabCol);
+    depthDistM = GetDistanceMatrix(meanDepth);
+    featDistM =  alpha * depthDistM + (1-alpha)  *colDistM;
 
-    smapName = fullfile(RES_Sali, strcat(noSuffixName, '.png'));
-    [partialImg,partialH,partialW] = ComputeSaliencyMap(optsalMap, pixelList, frameRecord,true);
+    sal = CalWeightedContrast(featDistM, posDistM, bgfocus);
+
     
-    partialImg = (partialImg-min(partialImg(:)))/(max(partialImg(:))-min(partialImg(:)));
-    partialImg = partialImg * 255;
-    partialImg = uint8(partialImg);
+    %% Saliency Optimization
+    [clipVal, geoSigma, neiSigma] = EstimateDynamicParas(adjcMatrix, featDistM);
+    [bgProb, bdCon, bgWeight] = EstimateBgProb(featDistM, adjcMatrix, bdIds, clipVal, geoSigma);
+    optsal = SaliencyOptimization(adjcMatrix, bdIds, featDistM, neiSigma, bgWeight, sal);
+    
+    [partialImg,partialH,partialW] = ComputeSaliencyMap(optsal, pixelList, frameRecord,true);
     
     h = frameRecord(1);
     w = frameRecord(2);
@@ -78,7 +119,5 @@ for k=1:length(files)
     
     
 end
-
-
 
 
